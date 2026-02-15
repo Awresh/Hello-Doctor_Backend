bcb835615ee58ea8dab159c02da669340e2852f9
Akash Roy <akash@Akashs-MacBook-Air.local>
2026-02-15 15:49:49 +0530

    add

--- src/models/menu/menu.model.js
+++
@@ -1,5 +0,0 @@
-import { Section } from './section.model.js';
-import { MenuItem } from './menu-item.model.js';
-import { BaseRoute } from './base-route.model.js';
-
-export { Section, MenuItem, BaseRoute };
--- src/routes/upload.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import upload from '../utils/upload.util.js';
-import { uploadFile } from '../controllers/upload.controller.js';
-
-const router = express.Router();
-
-router.post('/upload', upload.single('file'), uploadFile);
-
-export default router;
